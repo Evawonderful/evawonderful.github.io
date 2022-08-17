b925edcdb5b8dfc71a6e2eddc7b28ca014adb3a9
Osalor Wonderful Eva <osaloreva@gmail.com>
2022-08-17 15:41:47 +0100

    commit all files

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+    {
+      title: "Portfolio",
+      subtitle: "React, TailwindCSS",
+      description:
+        "A portfolio website",
+      image: "./lapwork.jpg",
+      link: "https://eva-portfolio.com",
+    },
+    {
+      title: "Polongo",
+      subtitle: "React and Java",
+      description:
+        "Developed the UI for the Polongo app, a social media application designed to engerder community development ",
+      image: "./codegirl.jpg",
+      link: "https://polongo.com",
+    },
+    {
+      title: "Addstore",
+      subtitle: "React, CSS",
+      description:
+        "An online store for customized printing of T-shirts, Tote-bags and other merch.",
+      image: "./store.jpg",
+      link: "https://addressedprints.com.ng",
+    },
+    {
+      title: "Smart Citi",
+      subtitle: "HTML, CSS, Material UI",
+      description:
+        "A web-based application for automated flood-risk assessment and waste management.",
+      image: "./citi.png",
+      link: "https://goofy-morse-c0b2e1.netlify.app/",
+    },
+  ];
+
+  export const skills = [
+   "React", "Javascript", "HTML", "CSS", "TailwindCSS", "Material UI"
+    
+  ] ;
+
+  export const testimonials = [
+    {
+      quote: "Testimonial",
+      name: "Nifemi Adeyemo",
+      company:
+        "Polongo Initiative",
+      image: "./femi.jpg",
+    },
+    {
+      quote: "Testimonial",
+      name: "Prof. Olanike Adeyemo",
+      company:
+        "Tsoriel Solutions Limited",
+      image: "./prof.jpg",
+    },
+    {
+      quote: "Testimonial",
+      name: "Dr. Aderonke Sakpere",
+      company:
+        "Tech Girls Club",
+      image: "./drsakpere.png",
+    },
+    {
+      quote: "Testimonial",
+      name: "Columnist",
+      company:
+        "Geohackathon",
+      image: "./colu.jpg",
+    },
+  ];
